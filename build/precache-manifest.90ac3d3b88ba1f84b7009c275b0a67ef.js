cf4cbc4f73347a3973bb26340c62bc39ab59969b
MarKork <53899290+MarKork@users.noreply.github.com>
2019-11-28 21:09:51 +0200

    Add files via upload

---
+++ build/precache-manifest.90ac3d3b88ba1f84b7009c275b0a67ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06039f25bf7bbaccce4a083a40b14652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "483319189c762df3b06c",
+    "url": "/static/css/main.dc6a018f.chunk.css"
+  },
+  {
+    "revision": "f2501b6548d33dab0ab1",
+    "url": "/static/js/2.d6966a2f.chunk.js"
+  },
+  {
+    "revision": "483319189c762df3b06c",
+    "url": "/static/js/main.4df92c27.chunk.js"
+  },
+  {
+    "revision": "91b16906825959e2e77e",
+    "url": "/static/js/runtime~main.cf10c6f3.js"
+  }
+]);
