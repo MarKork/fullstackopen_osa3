9282fc8f1ca55476f7e54a0cd57fcd0d0a9b9247
Mari Korkka <mari.korkka@gmail.com>
2019-12-06 23:49:13 +0200

    no message

---
+++ build/precache-manifest.6c2133beee17da5dfb8fdc00a2ffd24a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52457269f823febadff601c4a9c5509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1947a7f38be50a5250b1",
+    "url": "/static/css/main.dc6a018f.chunk.css"
+  },
+  {
+    "revision": "f2501b6548d33dab0ab1",
+    "url": "/static/js/2.d6966a2f.chunk.js"
+  },
+  {
+    "revision": "1947a7f38be50a5250b1",
+    "url": "/static/js/main.d10f3796.chunk.js"
+  },
+  {
+    "revision": "91b16906825959e2e77e",
+    "url": "/static/js/runtime~main.cf10c6f3.js"
+  }
+]);
