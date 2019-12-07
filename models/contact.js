aee692e1256f82a14b7deb17336061a53f9cac58
Mari Korkka <mari.korkka@gmail.com>
2019-12-07 09:52:18 +0200

    no message

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
@@ -14,32 +14,32 @@ mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const contactSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minlength: 3,
-      required:true,
-      unique: true
-    },
-    number: {
-      type: String,
-      minlength: 8,
-      required:true,
-      unique:true
-    } ,
-    id: {
-      type: Number
-    }
-  })
-  
-  contactSchema.plugin(uniqueValidator);
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required:true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required:true,
+    unique:true
+  } ,
+  id: {
+    type: Number
+  }
+})
 
-  contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-  
-  module.exports = mongoose.model('Contact', contactSchema)
+contactSchema.plugin(uniqueValidator);
+
+contactSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
